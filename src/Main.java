2934fb9ea64636ccdb01560eb28a6c3ea1dfdec5
daw1 <laurafelipe@elpuig.xeill.net>
2025-03-05 16:47:03 +0100

    S'han creat 4 mètodes nous.

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+       method1();
+        method2();
+        method3();
+        method4();
+
+
+    }
+    private static void method1(){
+
+    }
+    private static void method2(){
+
+    }
+    private static void method3(){
+
+    }
+    private static void method4(){
+
+    }
+}
